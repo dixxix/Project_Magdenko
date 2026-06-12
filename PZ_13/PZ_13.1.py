9b240ec629e2910458e02f1d15037e7364dbe06a
dixxix <yasamagdenko@gmail.com>
2026-06-12 20:40:27 +0300

    ПЗ 13

--- PZ_13/PZ_13.1.py
+++ PZ_13/PZ_13.1.py
@@ -1,24 +1,11 @@
 """В исходном текстовом файле(Dostoevsky.txt) найти все произведения писателя.
 Посчитать количество полученных элементов"""
 import re
+
 with open("Dostoevsky.txt", 'r', encoding='utf-8') as f:
     text = f.read()
-works_patterns = [
-    r'Преступление и наказание',
-    r'Идиот',
-    r'Бесы',
-    r'Братья Карамазовы',
-    r'Подросток',
-    r'Униженные и оскорблённые',
-    r'Игрок',
-    r'Записки из мертвого дома',
-    r'Записки из подполья',
-    r'Двойник',
-    r'Бедные люди',
-    r'Белые ночи'
-]
-pattern_str = '|'.join(works_patterns)
-p = re.compile(pattern_str)
+pattern = r'[«"\']([^»"\']+)[»"\']'
+p = re.compile(pattern)
 found = p.findall(text)
 unique_works = set(found)
 print(f"Найдено произведений: {len(unique_works)}")
